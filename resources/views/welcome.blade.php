c48677bfa1a43e6f2fc31aecea54f266b2971694
Muhammad Yama <m.yama@live.com>
2022-01-16 05:26:32 +0300

    test

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Muhammad Yama
                 </div>
 
                 <div class="links">
